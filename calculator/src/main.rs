1d314e8657159aea9a7f24c9dcf9fe1d89d27276
luizsouz404 <luiz.souz404@gmail.com>
2024-02-06 21:58:39 -0300

    study: create CLI calculator

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,3 +1,39 @@
+use std::env::{args, Args};
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Args = args();
+
+    // Args
+    let first = args.nth(1).unwrap();
+    let operator = args.nth(0).unwrap().chars().next().unwrap();
+    let second = args.nth(0).unwrap();
+
+    // Parsing
+    let first_number = first.parse::<f32>().unwrap(); // or let first_number: f32 = first.parse().unwrap();
+    let second_number = second.parse::<f32>().unwrap(); // or let second_number: f32 = second.parse().unwrap();
+
+    let result = operate(operator, first_number, second_number);
+    println!("{:?}", output(first_number, operator, second_number, result));
 }
+
+fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
+    match operator {
+        '+' => first_number + second_number,
+        '-' => first_number - second_number,
+        '/' => first_number / second_number,
+        '*' | 'x' | 'X' => first_number * second_number,
+        _ => panic!("Invalid operator used.")
+    }
+}
+
+fn output(first_number: f32, operator: char, second_number: f32, result: f32) -> String {
+    format!("{} {} {} = {}", first_number, operator, second_number, result)
+}
+
+// fn nth(&mut self, n: usize) -> Option<String> {
+//     // assume n = 0 ;
+//     // inner = ["1", "2"]
+//     self.inner.next() // output: "1"
+//     // calling next again results in second element
+//     self.inner.next() // output: "2"
+// }
